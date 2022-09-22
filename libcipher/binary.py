e098d89baec19cc79e958dd55e77817bb0733042
Daniel López Guimaraes <danielectra@outlook.com>
2022-09-22 19:12:43 +0100

    binary: Remove letter arrangement

--- libcipher/binary.py
+++ libcipher/binary.py
@@ -2,86 +2,26 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-UppercaseList = [ 'A', 'B', 'C',
-                  'D', 'E', 'F',
-                  'G', 'H', 'I',
-                  'J', 'K', 'L',
-                  'M', 'N', 'O',
-                  'P', 'Q', 'R',
-                  'S', 'T', 'U',
-                  'V', 'W', 'X',
-                  'Y', 'Z' ]
-
-LowercaseList = [ 'a', 'b', 'c',
-                  'd', 'e', 'f',
-                  'g', 'h', 'i',
-                  'j', 'k', 'l',
-                  'm', 'n', 'o',
-                  'p', 'q', 'r',
-                  's', 't', 'u',
-                  'v', 'w', 'x',
-                  'y', 'z' ]
-
-def convert_to_binary(character: str) -> str:
-    '''Convert a given character to binary'''
-    # For easier manage of bytes, apply the ISO-8859-1 encoding
-    character_bytes = bytearray(character, 'latin1')
-    binary = bin(character_bytes[0])
-
-    # Remove characters that don't belong in binary
-    binary = binary[2:]
-
-    # If character is ASCII, add extra zeroes
-    while len(binary) < 8:
-        binary = '0' + binary
-    
-    return binary
-
-def encrypt_binary(string: str, offset: int) -> str:
+def encrypt_binary(string: str) -> str:
     '''Encrypt a given string of text to binary'''
     cipher = ''
     for letter in string:
-        # Search if letter exists in lists
-        if letter in UppercaseList:
-            letter_number = UppercaseList.index(letter) + offset
-            
-            # Check if letter offset bypasses list limit
-            if letter_number >= len(UppercaseList):
-                letter_number -= len(UppercaseList)
-            
-            binary_character = convert_to_binary(UppercaseList[letter_number])
-            cipher += binary_character + ' '
-            continue
-        
-        if letter in LowercaseList:
-            letter_number = LowercaseList.index(letter) + offset
-            
-            # Check if letter offset bypasses list limit
-            if letter_number >= len(LowercaseList):
-                letter_number -= len(LowercaseList)
-            
-            binary_character = convert_to_binary(LowercaseList[letter_number])
-            cipher += binary_character + ' '
-            continue
+        # For easier manage of bytes, apply the ISO-8859-1 encoding
+        character_bytes = bytearray(letter, 'latin1')
+        binary = bin(character_bytes[0])
 
-        # Apply shift to numbers (DISABLED BY DEFAULT)
-        """ if letter.isnumeric():
-            new_number = int(letter) + offset
+        # Remove characters that don't belong in binary
+        binary = binary[2:]
 
-            # Make number only one digit
-            if new_number >= 10:
-                new_number -= 10
-            
-            cipher += str(new_number)
-            continue """
+        # If character is ASCII, add extra zeroes
+        while len(binary) < 8:
+            binary = '0' + binary
 
-        # If it's a non-listed character, pass it directly
-        binary_character = convert_to_binary(letter)
-        cipher += binary_character + ' '
+        cipher += binary + ' '
 
     return cipher
 
-def decrypt_binary(string: str, offset: int) -> str:
+def decrypt_binary(string: str) -> str:
     '''Decrypt a given binary text to a string'''
     # Add an ending space to detect the last character
     string += ' '
@@ -103,40 +43,6 @@ def decrypt_binary(string: str, offset: int) -> str:
             character_int = int(ciphered_text, 2)
             character = chr(character_int)
 
-            # Search if letter exists in lists
-            if character in UppercaseList:
-                letter_number = UppercaseList.index(character) - offset
-            
-                # Check if letter offset bypasses list limit
-                if letter_number < 0:
-                    letter_number += len(UppercaseList)
-
-                decipher += UppercaseList[letter_number]
-                ciphered_text = ''
-                continue
-            
-            if character in LowercaseList:
-                letter_number = LowercaseList.index(character) - offset
-            
-                # Check if letter offset bypasses list limit
-                if letter_number < 0:
-                    letter_number += len(LowercaseList)
-            
-                decipher += LowercaseList[letter_number]
-                ciphered_text = ''
-                continue
-
-            # Apply shift to numbers (DISABLED BY DEFAULT)
-            """ if character.isnumeric():
-                new_number = int(character) - offset
-
-                # Make number only one digit
-                if new_number < 0:
-                    new_number += 10
-            
-                decipher += str(new_number)
-                ciphered_text = ''
-                continue """
             decipher += character
             ciphered_text = ''
         else:
