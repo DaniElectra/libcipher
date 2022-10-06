8c7cd2d6f4ef854e5eb52fccb58229dfd6838d77
Daniel López Guimaraes <danielectra@outlook.com>
2022-10-06 17:08:26 +0100

    core: Add cipher with key (Vignère cipher)

--- libcipher/core.py
+++ libcipher/core.py
@@ -11,7 +11,7 @@ from .normal import *
 
 modules_list = {'base64', 'binary', 'hexadecimal', 'morse', 'normal'}
 
-def letter_arrangement(string: str, offset: int, undo: bool, numbers = bool) -> str:
+def letter_arrangement(string: str, offset: int, undo: bool, numbers: bool, key: str) -> str:
     '''Apply letter arrangement to string based on offset'''
     UppercaseList = [ 'A', 'B', 'C',
                       'D', 'E', 'F',
@@ -34,13 +34,35 @@ def letter_arrangement(string: str, offset: int, undo: bool, numbers = bool) -> 
                       'y', 'z' ]
     
     new_string = ''
+    key_letter_count = 0
+    key_offset = 0
+    
     for letter in string:
+        # If there's a key assigned, extract it's offset value
+        if key != "":
+            # Keep a count for extracting the key letters,
+            # and reset it when we reach the end of the key
+            if key_letter_count >= len(key):
+                key_letter_count = 0
+        
+            # Extract the key letter
+            letter_key = key[key_letter_count]
+            
+            # Check the letter offset in lists
+            if letter_key in UppercaseList:
+                key_offset = UppercaseList.index(letter_key)
+
+            if letter_key in LowercaseList:
+                key_offset = LowercaseList.index(letter_key)
+            
+            key_letter_count += 1
+
         # Search if letter exists in lists
         if letter in UppercaseList:
             if undo:
-                letter_number = UppercaseList.index(letter) - offset
+                letter_number = UppercaseList.index(letter) - offset - key_offset
             else:
-                letter_number = UppercaseList.index(letter) + offset
+                letter_number = UppercaseList.index(letter) + offset + key_offset
             
             # Check if letter offset bypasses list limits
             while letter_number >= len(UppercaseList):
@@ -54,9 +76,9 @@ def letter_arrangement(string: str, offset: int, undo: bool, numbers = bool) -> 
 
         if letter in LowercaseList:
             if undo:
-                letter_number = LowercaseList.index(letter) - offset
+                letter_number = LowercaseList.index(letter) - offset - key_offset
             else:
-                letter_number = LowercaseList.index(letter) + offset
+                letter_number = LowercaseList.index(letter) + offset + key_offset
             
             # Check if letter offset bypasses list limits
             while letter_number >= len(LowercaseList):
@@ -90,24 +112,24 @@ def letter_arrangement(string: str, offset: int, undo: bool, numbers = bool) -> 
     return new_string
 
 class Advanced:
-    '''Advanced functions for the library, like letter shift'''
-    def encrypt(string: str, type: str, offset = 0, numbers = False) -> str:
-        '''Encrypt a given string using the specified type and offset'''
+    '''Advanced functions for the library, like letter shift and keys'''
+    def encrypt(string: str, type: str, offset = 0, numbers = False, key = "") -> str:
+        '''Encrypt a given string using the specified type, offset and key'''
         # Check if module exists in modules list
         if type in modules_list:
             encrypt_dynamic = getattr(sys.modules[__name__], 'encrypt_' + type)
         else:
             raise NotImplemented
         
-        # Check if offset isn't zero. In that case, do letter arrangement
-        if offset != 0:
-            string = letter_arrangement(string, offset, False, numbers)    
+        # Check if offset or key given. In that case, do letter arrangement
+        if offset != 0 or key != "":
+            string = letter_arrangement(string, offset, False, numbers, key)    
 
         cipher = encrypt_dynamic(string)
         return cipher
 
-    def decrypt(string: str, type: str, offset = 0, numbers = False) -> str:
-        '''Decrypt a given string using the specified type and offset'''
+    def decrypt(string: str, type: str, offset = 0, numbers = False, key = "") -> str:
+        '''Decrypt a given string using the specified type, offset and key'''
         # Check if module exists in modules list
         if type in modules_list:
             decrypt_dynamic = getattr(sys.modules[__name__], 'decrypt_' + type)
@@ -116,17 +138,24 @@ class Advanced:
         
         decipher = decrypt_dynamic(string)
 
-        # Check if offset isn't zero. In that case, do letter arrangement
-        if offset != 0:
-            decipher = letter_arrangement(decipher, offset, True, numbers)
+        # Check if offset or key given. In that case, do letter arrangement
+        if offset != 0 or key != "":
+            decipher = letter_arrangement(decipher, offset, True, numbers, key)
 
         return decipher
 
-    def recrypt(input: str, input_type: str, output_type: str, input_offset = 0,
-                output_offset = 0, input_numbers = False, output_numbers = False) -> str:
-        '''Recrypt a given encoded string into other encoding, using given offsets'''
-        decrypted = Advanced.decrypt(input, input_type, input_offset, input_numbers)
-        recrypted = Advanced.encrypt(decrypted, output_type, output_offset, output_numbers)
+    def recrypt(input: str,
+                input_type: str,
+                output_type: str,
+                input_offset = 0,
+                output_offset = 0,
+                input_numbers = False,
+                output_numbers = False,
+                input_key = "",
+                output_key = "") -> str:
+        '''Recrypt a given encoded string into other encoding, using given offsets and keys'''
+        decrypted = Advanced.decrypt(input, input_type, input_offset, input_numbers, input_key)
+        recrypted = Advanced.encrypt(decrypted, output_type, output_offset, output_numbers, output_key)
         return recrypted
 
 def encrypt(string: str, type: str) -> str:
