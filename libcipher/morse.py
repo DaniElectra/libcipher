e72c67f1183079b20c7b09d7977b223de1be87f6
Daniel López Guimaraes <danielectra@outlook.com>
2022-10-23 17:06:55 +0100

    morse: Remove superfluous code
    This code was used previously for letter arrangement, but that job now
    is done by the core

--- libcipher/morse.py
+++ libcipher/morse.py
@@ -2,9 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import collections
-
-MorseDictionaryLetters = collections.OrderedDict({ 'A':'.-', 'B':'-...',
+MorseDictionary = { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
                     'I':'..', 'J':'.---', 'K':'-.-',
@@ -12,9 +10,8 @@ MorseDictionaryLetters = collections.OrderedDict({ 'A':'.-', 'B':'-...',
                     'O':'---', 'P':'.--.', 'Q':'--.-',
                     'R':'.-.', 'S':'...', 'T':'-',
                     'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..' })
-
-MorseDictionary = { '0':'-----', '1':'.----', '2':'..---',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '0':'-----', '1':'.----', '2':'..---',
                     '3':'...--', '4':'....-', '5':'.....',
                     '6':'-....', '7':'--...', '8':'---..',
                     '9':'----.', '.':'.-.-.-', ',':'--..--',
@@ -30,19 +27,8 @@ def encrypt_morse(string: str) -> str:
     cipher = ''
     for letter in string:
         if letter != ' ':
-            try:
-                # Check the letter at the dictionary and add the correct sequence to the cipher
-                letter_number = list(MorseDictionaryLetters.keys()).index(letter.upper())
-                
-                # If letter offset bypasses the dictionary length, count from the start
-                if letter_number >= len(MorseDictionaryLetters):
-                    letter_number -= len(MorseDictionaryLetters)
-
-                new_letter = list(MorseDictionaryLetters)[letter_number]
-                cipher += MorseDictionaryLetters[new_letter] + ' '
-            except ValueError:
-                # If the character isn't a letter, go to the normal dictionary
-                cipher += MorseDictionary[letter] + ' '
+            # Check the letter at the dictionary and add the correct sequence to the cipher
+            cipher += MorseDictionary[letter.upper()] + ' '
         else:
             # A space means a different character
             # A slash indicates a different word
@@ -54,8 +40,7 @@ def decrypt_morse(string: str) -> str:
     # Add an ending space to detect the last character
     string += ' '
 
-    # Create inverse dictionaries for easier lookup
-    InverseMorseDictionaryLetters = collections.OrderedDict({value: key for key, value in MorseDictionaryLetters.items()})
+    # Create inverse dictionary for easier lookup
     InverseMorseDictionary = {value: key for key, value in MorseDictionary.items()}
 
     decipher = ''
@@ -73,14 +58,7 @@ def decrypt_morse(string: str) -> str:
                 continue
 
             # Decode character using inverse dictionary
-            try:
-                letter_number = list(InverseMorseDictionaryLetters.keys()).index(ciphered_text)
-
-                new_letter = list(InverseMorseDictionaryLetters)[letter_number]
-                decipher += InverseMorseDictionaryLetters[new_letter]
-            except ValueError:
-                # If the character isn't a letter, go to the normal dictionary
-                decipher += InverseMorseDictionary[ciphered_text]
+            decipher += InverseMorseDictionary[ciphered_text]
             ciphered_text = ''
         else:
             # Store Morse code of single character
